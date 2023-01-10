a62d19b2aa3a607a9858810cd4dbf7900b727182
JadhavDnyaneshwarr <118913484+JadhavDnyaneshwarr@users.noreply.github.com>
2023-01-10 17:41:33 +0530

    Day 5 assignment.

--- src/com/bridgelabz/coreprograms/PowerOf2.java
+++ src/com/bridgelabz/coreprograms/PowerOf2.java
@@ -3,12 +3,9 @@ package com.bridgelabz.coreprograms;
 public class PowerOf2 {
     public static void main(String[] args) {
         int table = 2;
-        int value=1;
-        int j = 1;
-        for (int i = 30; i!=0; i-- ){
-            value *= table;
-            System.out.println("2 ^ " + j + "="+ value);
-            j++;
+        for (int i = 1; i<31; i++ ){
+            double value = Math.pow(table,i);
+            System.out.println("2 ^ " + i + "="+ value);
         }
     }
 }
