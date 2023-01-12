c737ea1d02a5594010d493c1514a447edc258063
JadhavDnyaneshwarr <118913484+JadhavDnyaneshwarr@users.noreply.github.com>
2023-01-12 19:48:04 +0530

    Day5-Assignment

--- src/com/bridgelabz/functionalprograms/WindChill.java
+++ src/com/bridgelabz/functionalprograms/WindChill.java
@@ -3,7 +3,9 @@ package com.bridgelabz.functionalprograms;
 public class WindChill {
     public static void main(String[] args) {
         int t =35;
+//        int t =Integer.parseInt(args[0]);
         int v =90;
+//        int v =Integer.parseInt(args[1]);
         double a = (0.4275 * t) - 35.75;
         double b = (double)Math.pow(v,0.16);
         double w =(double)( 35.74 + (0.6215 * t )+ a * b);
